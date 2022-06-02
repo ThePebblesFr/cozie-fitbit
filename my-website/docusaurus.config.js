d6d4cb5be6b103d268a8b26ccd4dbe8840bdd2f6
Pierre <pierre.garreau@etu.emse.fr>
2022-06-02 13:22:23 +0800

    frist commit github pages

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Cozie, a Fitbit Clockface for IEQ and physiological data collection',
   tagline: 'Allows building occupants to provide feedback in real time',
-  url: 'https://cozie-with-website.netlify.app',
+  url: 'https://thepebblesfr.github.io/cozie-fitbit',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
